4d39615e894f0da1c85af1a3b9bb77b818d432cf
henriquesdias <henrique.dias1915@gmail.com>
2023-01-20 03:05:36 -0300

    config: configures connection with database through prisma

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
 import express from "express";
 import loadEnv from "./config/envs.js";
+import { prisma } from "./config/database-postgres.js";
 
 loadEnv();
 
 const server = express();
 
+server.get("/", async (req, res) => {
+  try {
+    const users = await prisma.users.findMany();
+    res.send(users);
+  } catch (error) {
+    console.log(error);
+    res.send("error");
+  }
+});
 const PORT = process.env.PORT;
 
 server.listen(PORT, () => console.log(`Listening on port ${PORT}`));

6e6c583d71369e11c320dc4d1396f25585aff86b
henriquesdias <henrique.dias1915@gmail.com>
2023-02-01 02:34:04 -0300

    tests: configure tests to router '/create-post' with cases: token is not valid, token is not given and token is valid

--- tests/integrations/posts-test.ts
+++ tests/integrations/posts-test.ts
@@ -2,10 +2,12 @@ import supertest from "supertest";
 
 import server from "../../src";
 import { STATUS_CODE } from "../../src/helpers/status-code";
-import { cleanDatabase } from "../helpers";
+import { cleanDatabase, generateValidToken } from "../helpers";
 import { createPost } from "../factories/posts-factories";
 import { createUser } from "../factories/users-factories";
 import { createComment } from "../factories/comments-factories";
+import { faker } from "@faker-js/faker";
+import { prisma } from "../../src/config/database-postgres";
 
 beforeEach(async () => {
   await cleanDatabase();
@@ -53,5 +55,49 @@ describe("GET /", () => {
         ],
       },
     ]);
+  });
+});
+
+describe("POST /create-post", () => {
+  it("should respond with status 401 if no token is given", async () => {
+    await createUser();
+    const response = await api
+      .post("/create-post")
+      .send({ description: faker.random.words() });
+
+    expect(response.status).toBe(STATUS_CODE.UNAUTHORIZED);
+  });
+
+  it("should respond with status 401 if token is not valid", async () => {
+    await createUser();
+    const token = faker.random.alphaNumeric();
+    const response = await api
+      .post("/create-post")
+      .set("Authorization", `Bearer ${token}`);
+
+    expect(response.status).toBe(STATUS_CODE.UNAUTHORIZED);
+  });
+
+  describe("when token is valid", () => {
+    it("should respond with status 201 ,post data and add a new post on database", async () => {
+      const user = await createUser();
+      const token = generateValidToken(user);
+      const description = faker.random.words();
+      const response = await api
+        .post("/create-post")
+        .set("Authorization", `Bearer ${token}`)
+        .send({ description });
+
+      const post = await prisma.posts.findFirst();
+      expect(response.status).toBe(STATUS_CODE.CREATED);
+      expect(response.body).toMatchObject({
+        userId: user.id,
+        description,
+      });
+      expect(response.body).toMatchObject({
+        userId: post.userId,
+        description: post.description,
+      });
+    });
   });
 });

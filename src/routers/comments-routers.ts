2ad14220062578110be6067335cdaf4558dcb73c
henriquesdias <henrique.dias1915@gmail.com>
2023-02-11 16:16:14 -0300

    feat: isolates comments in a router '/comments/:postId'

--- src/routers/comments-routers.ts
+++ src/routers/comments-routers.ts
@@ -2,16 +2,21 @@ import { Router } from "express";
 
 import { authenticateToken } from "../middlewares/authentication-middleware";
 import { validateBody } from "../middlewares/validate-body";
-import createComment from "../controllers/comments-controllers";
+import {
+  createComment,
+  getComments,
+} from "../controllers/comments-controllers";
 import { schemaComment } from "../schemas/comment";
 
 const commentRouter = Router();
 
-commentRouter.post(
-  "/comments/:postId/",
-  authenticateToken,
-  validateBody(schemaComment),
-  createComment
-);
+commentRouter
+  .post(
+    "/comments/:postId/",
+    authenticateToken,
+    validateBody(schemaComment),
+    createComment
+  )
+  .get("/comments/:postId", getComments);
 
 export { commentRouter };
